c03b0eb741c6188ad57fe97a0430259764da2fe8
Sebastian <naesebastian96@gmail.com>
2025-07-13 01:18:11 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [RouterOutlet, MatSlideToggleModule]
 })
 export class AppComponent {
   title = 'angular-reboot-dashboard';
