9f27c4eb7321bccb4b2eb4dc7cd4fb7f0dc2ea18
Sebastian <naesebastian96@gmail.com>
2025-07-13 17:07:03 +0300

    added authentication shell component

---
+++ src/app/features/authentication/main-components/authentication-shell/authentication-shell.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-authentication-shell',
+  standalone: true,
+  imports: [],
+  templateUrl: './authentication-shell.component.html',
+  styleUrl: './authentication-shell.component.scss'
+})
+export class AuthenticationShellComponent {
+
+}
