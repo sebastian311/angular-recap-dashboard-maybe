b4dc4ef96874613d974366463347a0cbcd199c78
Sebastian <naesebastian96@gmail.com>
2025-07-27 17:23:05 +0300

    Added route guard to dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './shared/services/auth.guard';
 
 export const routes: Routes = [
     // TODO: Later on, we should add a wildcard route here,
@@ -8,5 +9,8 @@ export const routes: Routes = [
         pathMatch: 'full', 
     },
     { path: 'login', loadComponent: () => import('./features/authentication/main-components/authentication-shell/authentication-shell.component').then(c => c.AuthenticationShellComponent)},
-    { path: 'dashboard', loadComponent: () => import('./features/dashboard/main-components/dashboard-shell/dashboard-shell.component').then(c => c.DashboardShellComponent) },
+    { path: 'dashboard', 
+        loadComponent: () => import('./features/dashboard/main-components/dashboard-shell/dashboard-shell.component').then(c => c.DashboardShellComponent),
+        canActivate: [authGuard]
+    },
 ];
